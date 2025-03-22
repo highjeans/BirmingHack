b62ce9e318d4fb7bc7f763200da4695d01e95681
highjeans <vivan.waghela23@gmail.com>
2025-03-22 16:58:57 +0000

    Update files

--- shared/src/lib.rs
+++ shared/src/lib.rs
@@ -1,18 +1,4 @@
-use rust_bert::pipelines::sentence_embeddings::{
-    SentenceEmbeddingsBuilder, SentenceEmbeddingsModelType,
-};
 use serde::{Deserialize};
-
-fn example() -> Option<(String)> {
-    let model = SentenceEmbeddingsBuilder::remote(SentenceEmbeddingsModelType::AllMiniLmL12V2)
-        .create_model()
-        .ok()?;
-
-    let sentences = ["this is an example sentence", "each sentence is converted"];
-
-    let output = model.encode(&sentences).ok()?;
-    Some(format!("{output:?}"))
-}
 
 #[derive(Deserialize)]
 pub struct SignupData {
@@ -25,14 +11,4 @@ pub struct SignupData {
 pub struct LoginData {
     pub username: String,
     pub password: String,
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    //#[test]
-    //fn it_works() {
-    //    assert_eq!(example().unwrap_or_default(), String::from("69"));
-    //}
 }
