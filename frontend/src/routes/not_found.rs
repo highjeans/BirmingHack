674e337a80c1710918cefe7ea6fa27242606afab
Freddy Snow <git@fjsn.io>
2025-03-22 16:26:13 +0000

    Layout

--- frontend/src/routes/not_found.rs
+++ frontend/src/routes/not_found.rs
@@ -2,5 +2,7 @@ use leptos::prelude::*;
 
 #[component]
 pub fn NotFound() -> impl IntoView {
-    view! { "404" }
+    view! { <div class="flex items-center justify-center flex-grow">
+        <h1 class="text-4xl font-black">404</h1>
+    </div> }
 }
