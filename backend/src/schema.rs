6ca0728d07e0ac4ecd395ed417875a20bd00b3fa
highjeans <vivan.waghela23@gmail.com>
2025-03-22 14:00:04 +0000

    Create socials and books tables

--- backend/src/schema.rs
+++ backend/src/schema.rs
@@ -1,8 +1,38 @@
+// @generated automatically by Diesel CLI.
+
 diesel::table! {
-    users {
+    books (isbn) {
+        isbn -> Int8,
+        title -> Text,
+        author -> Text,
+        embeddings -> Text,
+        user_id -> Uuid,
+    }
+}
+
+diesel::table! {
+    socials (id) {
+        id -> Uuid,
+        platform -> Text,
+        username -> Text,
+        user_id -> Uuid,
+    }
+}
+
+diesel::table! {
+    users (id) {
         id -> Uuid,
         username -> Text,
         password -> Text,
         fullname -> Text,
     }
 }
+
+diesel::joinable!(books -> users (user_id));
+diesel::joinable!(socials -> users (user_id));
+
+diesel::allow_tables_to_appear_in_same_query!(
+    books,
+    socials,
+    users,
+);
