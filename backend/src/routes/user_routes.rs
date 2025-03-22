ef648e8dd9a74737a78b235d41d52d6deaabb3b2
highjeans <vivan.waghela23@gmail.com>
2025-03-22 17:58:09 +0000

    Add working login backend

--- backend/src/routes/user_routes.rs
+++ backend/src/routes/user_routes.rs
@@ -1,9 +1,19 @@
-use rocket::{serde::json::Json, http::Status};
+use bcrypt::BcryptError;
+use rocket::http::CookieJar;
+use rocket::{serde::{Deserialize, Serialize, json::Json}, http::Status};
 use rocket_db_pools::{Connection, diesel::prelude::*};
-use shared::{LoginData, SignupData};
+use shared::{LoginData, LoginResponse, SignupData};
+use jsonwebtoken::{encode, Header, EncodingKey};
+use std::time::{Duration, SystemTime, UNIX_EPOCH};
 use crate::database_structs::Users::Users;
-
 use crate::Db;
+
+#[derive(Serialize, Deserialize)]
+#[serde(crate = "rocket::serde")]
+struct Claims {
+    expires_at: u128,
+    id: String
+}
 
 #[post("/signup", format = "json", data = "<details>")]
 pub async fn signup(details: Json<SignupData>, mut db: Connection<Db>) -> Status {
@@ -26,28 +36,52 @@ pub async fn signup(details: Json<SignupData>, mut db: Connection<Db>) -> Status
 }
 
 #[post("/login", format = "json", data = "<details>")]
-pub async fn login(details: Json<LoginData>, mut db: Connection<Db>) {
+pub async fn login(details: Json<LoginData>, mut db: Connection<Db>, cookies: &CookieJar<'_>) -> (Status, Json<LoginResponse>) {
     use crate::schema::users::dsl::*;
     match users.filter(username.eq(details.username.clone())).select(Users::as_select()).load(&mut db).await {
-        Ok(user) => {
-            if user.is_empty() {
-                // TODO: return that the user was not found
-                todo!();
+        Ok(user_list) => {
+            if user_list.is_empty() {
+                return (Status::Forbidden, Json(LoginResponse {
+                    message: "Invalid username".to_string(),
+                }));
             }
-            match bcrypt::verify(details.password.clone(), user.get(0).unwrap().password.as_str()) {
-                Ok(_) => {
-                    // TODO: Create secure cookie and return message
-                    todo!()
+            let user = user_list.get(0).unwrap();
+            let claims = Claims {
+                expires_at: (SystemTime::now() + Duration::new(60*60*24*7, 0)).duration_since(UNIX_EPOCH).unwrap().as_millis(),
+                id: user.id.to_string()
+            };
+            match bcrypt::verify(details.password.clone(), user.password.as_str()) {
+                Ok(matched) => {
+                    if !matched {
+                        return (Status::Forbidden, Json(LoginResponse {
+                            message: "Invalid Password".to_string(),
+                        }));
+                    }
+                    match encode(&Header::default(), &claims, &EncodingKey::from_secret(std::env::var("JWT_KEY").unwrap().as_bytes())) {
+                        Ok(jwt) => {
+                            cookies.add_private(("jwt", jwt.clone()));
+                            (Status::NoContent, Json(LoginResponse {
+                                message: "".to_string()
+                            }))
+                        },
+                        Err(_) => {
+                            (Status::InternalServerError, Json(LoginResponse {
+                                message: "Could not create jwt.".to_string(),
+                            }))
+                        },
+                    }
                 },
-                Err(_) => {
-                    // TODO: Return that password is invalid
-                    todo!();
+                Err(err) => {
+                    (Status::InternalServerError, Json(LoginResponse {
+                        message: err.to_string(),
+                    }))
                 },
             }
         },
         Err(err) => {
-            let res = err.to_string();
-            // TODO: return error message with InternalServerError
+            (Status::InternalServerError, Json(LoginResponse {
+                message: err.to_string(),
+            }))
         },
     }
 }
