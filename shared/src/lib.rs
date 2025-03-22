9d403b53860223443fb1627d88549c899595b4dc
James <jameskowal10@gmail.com>
2025-03-22 14:29:21 +0000

    Add example test

--- shared/src/lib.rs
+++ shared/src/lib.rs
@@ -1,24 +1,43 @@
+use std::iter::zip;
+
 use rust_bert::pipelines::sentence_embeddings::{
     SentenceEmbeddingsBuilder, SentenceEmbeddingsModelType,
 };
 
-fn example() -> Option<(String)> {
+fn dissimilar() -> Option<(f32, f32)> {
     let model = SentenceEmbeddingsBuilder::remote(SentenceEmbeddingsModelType::AllMiniLmL12V2)
         .create_model()
         .ok()?;
-
-    let sentences = ["this is an example sentence", "each sentence is converted"];
-
+    let sentences = [
+        "this is an example sentence",
+        "each sentence is converted",
+        "this is a dissimilar, different, weird version",
+    ];
     let output = model.encode(&sentences).ok()?;
-    Some(format!("{output:?}"))
+    Some((
+        cosine_similarity(&output[0], &output[1]),
+        cosine_similarity(&output[1], &output[2]),
+    ))
+}
+
+fn dot_prod(v: &Vec<f32>, u: &Vec<f32>) -> f32 {
+    zip(v, u).map(|(v1, u1)| v1 * u1).sum()
+}
+
+fn cosine_similarity(v: &Vec<f32>, u: &Vec<f32>) -> f32 {
+    let dp = dot_prod(v, u);
+    let norm_v: f32 = v.iter().map(|v| v.powi(2)).sum();
+    let norm_u: f32 = u.iter().map(|u| u.powi(2)).sum();
+    dp / norm_v.sqrt() * norm_u.sqrt()
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    //#[test]
-    //fn it_works() {
-    //    assert_eq!(example().unwrap_or_default(), String::from("69"));
-    //}
+    #[test]
+    fn it_works() {
+        let (sim, disim) = dissimilar().unwrap();
+        assert!(sim > disim)
+    }
 }
