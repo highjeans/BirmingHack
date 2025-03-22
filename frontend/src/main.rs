5ec36b4b118f56c77ac9cfe7f378edb90fdf3b20
Freddy Snow <git@fjsn.io>
2025-03-22 14:26:59 +0000

    router

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,5 +1,31 @@
 use leptos::prelude::*;
+use leptos_router::components::{ParentRoute, Route, Router, Routes};
+use leptos_router_macro::path;
+
+mod routes;
+
+#[component]
+pub fn App() -> impl IntoView {
+    view! {
+        <Router>
+            <nav></nav>
+            <main>
+                <Routes fallback=|| routes::not_found::NotFound>
+                    <Route path=path!("/") view=routes::index::Index/>
+                    <Route path=path!("/login") view=|| view! { <h1>Not yet implemented.</h1> }/>
+                    <Route path=path!("/register") view=|| view! { <h1>Not yet implemented.</h1> }/>
+                    <ParentRoute path=path!("/profile") view=|| view! { <h1>Not yet implemented.</h1> }>
+                        <Route path=path!("/:id") view=|| view! { <h1>Not yet implemented.</h1> }/>
+                    </ParentRoute>
+                    <ParentRoute path=path!("/listing") view=|| view! { <h1>Not yet implemented.</h1> }>
+                        <Route path=path!("/:id") view=|| view! { <h1>Not yet implemented.</h1> }/>
+                    </ParentRoute>
+                </Routes>
+            </main>
+        </Router>
+    }
+}
 
 fn main() {
-    leptos::mount::mount_to_body(|| view! { <p>"Hello, world!"</p> })
+    leptos::mount::mount_to_body(App);
 }
